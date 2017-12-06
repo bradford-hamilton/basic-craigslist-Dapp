783c3af9f6caa6496cb0c4fb857f5bea8478c220
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2017-12-06 07:31:19 -0700

    [feature] add rinkeby and live network configs to truffle.js

--- truffle.js
+++ truffle.js
@@ -1,9 +1,21 @@
 module.exports = {
   networks: {
     development: {
-      host: "localhost",
+      host: 'localhost',
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      host: 'localhost',
+      port: 8545,
+      network_id: 4
+    },
+    live: {
+      host: 'localhost',
+      port: 8545,
+      network_id: 1,
+      from: '',
+      gas: 2000000,
     }
   }
 };
